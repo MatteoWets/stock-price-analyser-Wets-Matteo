74f3465f92da5eefa2ac4ab9695a62d89e826fc0
Matteo Wets <matteowets@mac.home>
2025-12-20 15:20:00 +0100

    final modifications

--- src/main.py
+++ src/main.py
@@ -15,11 +15,8 @@ EXCLUDE = {
     "test_models.py",
     "feature_engineeringV1.py",
     "features_comparison.py",
-<<<<<<< HEAD
     "final_visualizations.py"
-=======
-    "final_visualitzation.py"
->>>>>>> 8197f3c8b1bc4b6c148354d233c9296efa9890e9
+
 }
 
 def run_all_scripts(dry_run=False):
